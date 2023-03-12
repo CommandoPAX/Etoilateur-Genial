a3847253b43c27c634285e73acaa7fc92e1fcf4b
casse-stylo <125504937+casse-stylo@users.noreply.github.com>
2023-03-12 10:49:08 +0100

    Update EtoileDF.py

--- EtoileDF.py
+++ EtoileDF.py
@@ -15,16 +15,16 @@ else:
 sns.set(color_codes = True)
 sns.set(font_scale=1.5,font="Ubuntu") # fixe la taille de la police à 1.5 * 12pt
 
-"""Programme qui parcourt les fichiers générés par Genec et Starevol
+""" Programme qui parcourt les fichiers générés par Genec et Starevol
 Permet entre autres de générer le diagramme HR ou l'évolution d'un paramètre au cours du temps, ainsi que renvoyer l'intégralité des paramètres a un age donné
 F Castillo et T Bruant 2023
 Conventions pour les données et leurs unités :
 Luminosité : L | L/Lo
 Rayon : R | R/Ro
-Température : T | T/To
+Température : T | K
 Masse : M | M/Mo
 temps/age : t | annees
-"""
+ABondances : fractions massiques """
 
 class Structure (object):
     def __init__ (self, modele, source):
@@ -32,11 +32,13 @@ class Structure (object):
         
         self.modele = modele
         self.source = source
-        self.test = "Erreur" #Fonction test
+
+        # Homogénéisation de la notation Genec-Starevol
+
         self.elts = ["n","X","2H","3He","Y","6Li","7Li","9Be","10B","11B",
             "12C","13C","14C","14N","15N","15O","16O","17O","18O","18F","19F","20Ne",
             "21Ne","22Ne","23Na","24Mg","25Mg","26Mg","27Al", "Alg26","28Si", "32S", 
-            "36Ar", "Ca40", "44Ti", "48Cr", "52Fe", "56Ni"] #Homogénéisation de la notation Genec-Starevol
+            "36Ar", "Ca40", "44Ti", "48Cr", "52Fe", "56Ni"]     
         self.elts_sv = ["n","H1","H2","He3","He4","Li6","Li7","Be9","B10","B11",
             "C12","C13","C14","N14","N15","O15","O16","O17","O18","F18","F19","Ne20",
             "Ne21","Ne22","Na23","Mg24","Mg25","Mg26","Al27", "26Alg","Si28", "S32", 
@@ -53,43 +55,41 @@ class Structure (object):
 
             fichier = open(self.source,'r')
             
-            Data = ""
+            Data = fichier.read()
 
-            fichier.readline() #enlève les deux premières lignes sans données intéressantes
-            fichier.readline()
-            while 1 : #J'aime le danger
-                line = fichier.readline() #lis les lignes
+            for j in range(10): # Vire les espaces de merde
+                Data = Data.replace("  "," ")
+                Data = Data.replace("\n ","\n")
+                Data = Data.replace(" \n","\n")
+                Data = Data.replace("#","")
 
-                if line == "" : break #On a toujours une ligne vide à la fin, sert de check pour la fin de la boucle
-
-                for j in range(10): #Vire les espaces de merde
-                    line = line.replace("  "," ")
-                    line = line.replace("\n ","\n")
-                    line = line.replace(" \n","\n")
-                    line = line.replace("#","")
-                Data += line
-
-            for k in self.dico_elts : #change le nom des élements pour être en accord avec les conventions
+            for k in self.dico_elts : # Change le nom des élements pour être en accord avec les conventions
                     Data = Data.replace(k,self.dico_elts[k])
 
             Data=StringIO(Data)
             
-            self.DF = pd.read_csv(Data, delimiter = " ") #crée le dataframe ou sont stocké les données dans le format standard. Attention : pas d'index
+            self.DF = pd.read_csv(Data, delimiter = " ",skiprows = [0,1]) # Crée le dataframe ou sont stocké les données dans le format standard. Attention : pas d'index
             self.test = self.DF
             
             self.args = {}        
 
-            for i in self.DF.columns : #Passe les colonnes en array numpy, plus rapide pour les calculs
+            for i in self.DF.columns : # Passe les colonnes en array numpy, plus rapide pour les calculs
                 if not i in self.args : self.args[i] = np.array(self.DF[i])
 
             fichier.close()
+
+            self.args["r"] = 10**self.args["r"]/6.96e10
+            self.args["T"] = 10**self.args["t"]
+            self.args["P"] = 10**self.args["p"]
+            self.nablad = self.args["Nabad"]
+            self.nablarad = self.args["Nabrad"]
         
         if self.modele == "Starevol":
                 
             fichiers = [0,0,0,0,0,0,0]
             DFS = [0,0,0,0,0,0,0]
                     
-            for root,dirs,files in os.walk(self.source): #ouvre tout les fichiers utiles
+            for root,dirs,files in os.walk(self.source): # Ouvre tout les fichiers utiles
                 for file in files:
                     if not ".gz" in file :
                         if (".p1" in file and not ".p10" in file
@@ -134,15 +134,30 @@ class Structure (object):
 
             for i in self.DF.columns :
                 if not i in self.args : self.args[i] = np.array(self.DF[i])
+
             for k in fichiers : k.close()
 
-    def Evolution (self,parametre,legende,X = "R", couleur="black",masse = True):
-        if masse : legende += " ; M = "+str(self.vraiemasse[0])+" Mo"
+            self.nablad = self.args["abadd"]
+            self.nablarad = self.args["abrad"]
 
-        plt.plot(self.args[X],self.args[parametre], label=legende,color=couleur)
+    def __getitem__ (self,x): # Permet d'accéder à un paramètre directement en tapant Etoile["parametre"]
+        return self.args[x]
 
-        axes.set_xlabel(X)
-        axes.set_ylabel(parametre)
+    def Evolution (self,parametres,legende,X = "r", masse = True,xlegende="",ylegende="",show=False):
+
+        if type(parametres) == str : parametres = [parametres]
+
+        if xlegende == "" : xlegende = X
+
+        for y in parametres :
+            plt.plot(self.args[X],self.args[y], label=legende+" ; "+y)
+
+        axes.set_xlabel(xlegende)
+        axes.set_ylabel(ylegende)
+
+        plt.legend()
+
+        if show : plt.show()
 
     def Convection (self) :
         for i in range(len(self.nablad)):
@@ -150,21 +165,16 @@ class Structure (object):
             nablarad= self.nablarad[i]
 
             if abs(nablad-nablarad) < 0.01 :
-                plt.axvline(x = self.R[i],color='gray',linestyle='--',label="nablad=nablarad")
-                return self.R[i]
+                plt.axvline(x = self.args["r"][i],color='gray',linestyle='--',label="nablad=nablarad")
+                return self["r"][i]
             
-    def Test (self) : #Utilisé pour le débugging, inutile sinon
-        print(self.test)
-
 class Etoile (object): 
     def __init__(self, modele, source):
         
         # Définition de la classe Etoile
         # source est un fichier pour Genec, et le repertoire qui contient tous les fichiers pour Starevol (il faut les dezipper)
-        # Pour accéder à l'abondance au coeur du carbone 12 (par exemple) > Etoile.abondances_coeur["12C"]
+        # Pour accéder à l'abondance au coeur du carbone 12 (par exemple) > Etoile["12C_coeur"]
         
-        self.DF = pd.DataFrame() #Création du dataframe ou seront stocké les données, index par rapport au temps
-
         if modele not in ("Genec","Starevol") :
             raise ValueError ("Le modele doit etre Genec ou Starevol")
         
@@ -177,7 +187,7 @@ class Etoile (object):
         elts = ["X","2H","3He","Y","6Li","7Li","7Be","9Be","10B","11B",
             "12C","13C","14C","14N","15N","15O","16O","17O","18O","19F","20Ne",
             "21Ne","22Ne","23Na","24Mg","25Mg","26Mg","26Alm","26Alg","27Al","28Si","29Si",
-            "30Si","31P","32S","33S","34S","35S","35Cl","36S","36Cl","37Cl","heavy"] #liste de tous les éléments considérés
+            "30Si","31P","32S","33S","34S","35S","35Cl","36S","36Cl","37Cl","heavy"] # Liste de tous les éléments considérés
         
         self.abondances_surf={}
         self.abondances_coeur={}
@@ -220,8 +230,10 @@ class Etoile (object):
                 try :
                     self.abondances_surf[i]=np.array(self.DF[i+"_surf"])
                     self.abondances_coeur[i]=np.array(self.DF[i+"_coeur"])
-                except KeyError : #Il n'y a pas tous les éléments dans Genec
+                except KeyError : # Il n'y a pas tous les éléments dans Genec
                     pass
+
+            # Conversion des variables les plus utiles dans les mêmes unités pour les deux modèles
                 
             self.T = 10**np.array(self.DF["log(Teff)"])
             self.L = 10**np.array(self.DF["log(L/Lo)"])
@@ -236,7 +248,7 @@ class Etoile (object):
             fichiers = [0,0,0,0,0,0,0,0,0]
             DFS = [0,0,0,0,0,0,0,0,0]
                     
-            for root,dirs,files in os.walk(source): #ouvre tout les fichiers utiles
+            for root,dirs,files in os.walk(source): # Ouvre tout les fichiers utiles
                 for file in files :
                     if not ".gz" in file :
                         if "c1" in file : fichiers[5] = open(os.path.join(root,file),"r")
@@ -280,6 +292,8 @@ class Etoile (object):
             self.t = np.array(self.DF["t"])
             self.M = np.array(self.DF["M"])
 
+            # A optimiser avec le dico
+
             for e in elts :
                 if e == "X" :
                     self.abondances_coeur[e] = np.array(self.DF_coeur["H1"]) 
@@ -292,14 +306,14 @@ class Etoile (object):
                     self.abondances_surf[e]  = np.array(self.DF_surf["heavy"])
                 else:
                     try :
-                        self.abondances_coeur[e] = np.array(self.DF_coeur[e[-1:]+e[:-1]])   #transforme par exemple 12C en C12
+                        self.abondances_coeur[e] = np.array(self.DF_coeur[e[-1:]+e[:-1]])   # Transforme par exemple 12C en C12
                         self.abondances_surf[e]  = np.array(self.DF_surf[e[-1:]+e[:-1]])
                     except KeyError:
                         try :
-                            self.abondances_coeur[e] = np.array(self.DF_coeur[e[-2:]+e[:-2]])   #idem pour les elts de deux lettres, par exemple He
+                            self.abondances_coeur[e] = np.array(self.DF_coeur[e[-2:]+e[:-2]])   # Idem pour les elts de deux lettres, par exemple He
                             self.abondances_surf[e]  = np.array(self.DF_surf[e[-2:]+e[:-2]])
                         except KeyError:
-                            pass    #ca bugue encore pour Al26m, flemme de réfléchir
+                            pass    # Ca bugue encore pour Al26m, flemme de réfléchir
 
         self.Z_coeur = 1- self.abondances_coeur["X"]-self.abondances_coeur["Y"]
         self.Z_surf = 1- self.abondances_surf["X"]-self.abondances_surf["Y"]
@@ -320,7 +334,7 @@ class Etoile (object):
             self.args[e+"_surf"] = self.abondances_surf[e]
             self.args[e+"_coeur"] = self.abondances_coeur[e]
         
-        for i in self.DF.columns :                       # Rajout des nouvelles variables qu'on sait pas à quoi elles servent
+        for i in self.DF.columns :                          # Rajout des nouvelles variables qu'on sait pas à quoi elles servent
             if not i in self.args : self.args[i] = np.array(self.DF[i])
 
         self.M_ini = self.M[0]
@@ -328,7 +342,10 @@ class Etoile (object):
 
         self.DF.set_index("t", inplace = True)
 
-    def HR (self,couleur="black",legende="graphique",masse = True,Zini = False,axes ="",show = False): #Définit la fonction qui trace les diagrammes HR
+    def __getitem__ (self,x):           # Permet d'accéder à un paramètre directement en tapant Etoile["parametre"]
+        return self.args[x]
+    
+    def HR (self,couleur="black",legende="graphique",masse = True,Zini = False,axes ="",show = False): # Définit la fonction qui trace les diagrammes HR
 
         if axes == "" : 
             axes = plt.gca()
@@ -351,7 +368,7 @@ class Etoile (object):
             xlegend = Varx
             if logx : xlegend ="log "+xlegend
             
-        if type(Vary) == str : Vary = [Vary] #Verifie le bon type de données
+        if type(Vary) == str : Vary = [Vary] 
 
         X = self.args[Varx]
         if logx : X = np.log10(X)
@@ -371,46 +388,27 @@ class Etoile (object):
 
         if show : plt.show()
             
-    def Para (self, age, parametres): #Affiche les valeurs de certains parametres à un age donné. Prend une liste de str comme argument
+    def Para (self, age, parametres): # Affiche les valeurs de certains parametres à un age donné
 
-        index_DF = list(self.DF.index.values.tolist())
-        if type(parametres) != list : print("Mauvais type de données")
+        age_opti = np.argmin(np.abs(self.t - age))
 
-        varage = age - index_DF[0]
-        varage_pre = varage #evite un crash si le programme stop à l'itération 0
-        i = 1
-        while varage > 0 : #cherche les plus proches valeurs
-            varage_pre = varage
-            varage = age - index_DF[i]
-            i += 1
+        if type (parametres) == str : parametres = [parametres]
 
-        if abs(varage) < abs(varage_pre) : i = i-1
-        if abs(varage) > abs(varage_pre) : i = i-2
-        if abs(varage) == abs(varage_pre) : i = i-1
+        resultats = []
+        for k in parametres : resultats.append(self.args[k][age_opti])
 
-        print("t : ", index_DF[i]) #Affiche l'âge trouvé
+        return resultats
 
-        for para in parametres : #Affiche tout les paramètres
-            try :
-                print(para, ": ", self.DF[para].iloc[i])
-            except :
-                print("Erreur : ", para),
-                pass
+    def Age (self, X):      # Donne l'âge auquel l'étoile atteint une certaine abondance en hydrogène au coeur
 
-    def Age (self, X, err = 0.01):
+        x_opti = np.argmin(np.abs(self["X_coeur"]-X))
 
-        i = 0
+        return self.t[x_opti]
 
-        for x in self.abondances_coeur["X"] :
-            if abs(x-X) < err: break
-            i +=1
-
-        return self.t[i]
-
-    def Rien (self):    #fonction qui ne fait rien
+    def Rien (self):    # Fonction qui ne fait rien
         pass
     
-    def Spammer_Aaron (self):
+    def Spammer_Aaron (self):       # Permet d'enregistrer plein de graphes qu'on peut envoyer aux collègues pour les spammer
         for k in self.DF.columns :
             try:
                 self.Evolution(Varx="t",Vary=k,ylegend=k,logx=True,label="Aaron",show=False)
@@ -421,7 +419,10 @@ class Etoile (object):
 if __name__ == "__main__" :
 
     axes = plt.gca()
-    axes.invert_xaxis()
+    Struct = Structure(modele="Genec", source="Fichiers_structure/classique_m1.0.v1")
+    StructSV = Structure (modele = "Starevol", source = "Structure_M1")
+    
+    Struct.Evolution(parametres=["12C","14N","16O"],X="r",show=False,legende="Pas de rotation")
+    StructSV.Evolution(parametres=["12C","14N","16O"],X="r",show=False,legende="Rotation")
 
-    Sol = Etoile(modele = "Genec", source = "T:\Cours\DATA\GENEC\classique_m1.0.wg")
-    Sol.Para(570000, ["X_coeur", "Y_coeur"])
+    plt.show()
